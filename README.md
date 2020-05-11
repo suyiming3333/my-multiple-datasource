07d5b5271ed3fe52843faacd5cec812d6c670ec8
suyiming <suyiming3333@gmail.com>
2020-05-11 23:26:26 +0800

    multipledatasources5

--- README.md
+++ README.md
@@ -5,9 +5,13 @@ multipledatasources：sql 脚本
 
 multipledatasources2：
 通过分包实现多数据源的，但事务上存在问题，事务管理器只能处理主数据源的事务
+0511 配置不同数据源各自的事务管理器，手动处理事务
 
 multipledatasources3：
 通过分包扫描，定义不同的两个数据库，利用jta-atomikos解决传统(单应用)项目多数据源事务管理问题
 
 multipledatasources4：
 通过aop的方式实现动态数据库切换+jta-atomikos(手动、注解自动处理多数据源事务问题)
+
+multipledatasources5：
+通过aop的方式实现动态数据库切换，事务管理器通过动态数据源创建，默认只处理主数据源的事务
---
+++ multipledatasources5/src/main/java/com/sym/multipledatasources/datasource/DataSourceConfig.java
@@ -0,0 +1,65 @@
+package com.sym.multipledatasources.datasource;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@MapperScan(basePackages = "com.sym.multipledatasources.mapper", sqlSessionFactoryRef = "SqlSessionFactory")
+public class DataSourceConfig {
+	@Primary
+	@Bean(name = "test1DataSource")
+	@ConfigurationProperties(prefix = "spring.datasource.test1")
+	public DataSource getDateSource1() {
+		return DataSourceBuilder.create().build();
+	}
+
+	@Bean(name = "test2DataSource")
+	@ConfigurationProperties(prefix = "spring.datasource.test2")
+	public DataSource getDateSource2() {
+		return DataSourceBuilder.create().build();
+	}
+
+	@Bean(name = "dynamicDataSource")
+	public DynamicDataSource DataSource(@Qualifier("test1DataSource") DataSource test1DataSource,
+			@Qualifier("test2DataSource") DataSource test2DataSource) throws SQLException {
+		Map<Object, Object> targetDataSource = new HashMap<>();
+		targetDataSource.put(DataSourceType.DataBaseType.TEST01, test1DataSource);
+		targetDataSource.put(DataSourceType.DataBaseType.TEST02, test2DataSource);
+
+		DynamicDataSource dataSource = new DynamicDataSource();
+		dataSource.setTargetDataSources(targetDataSource);
+		dataSource.setDefaultTargetDataSource(test1DataSource);
+		return dataSource;
+	}
+
+	@Bean(name = "SqlSessionFactory")
+	public SqlSessionFactory test1SqlSessionFactory(@Qualifier("dynamicDataSource") DataSource dynamicDataSource)
+			throws Exception {
+		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+		bean.setDataSource(dynamicDataSource);
+		bean.setMapperLocations(
+				new PathMatchingResourcePatternResolver().getResources("classpath*:mapping/*.xml"));
+		return bean.getObject();
+	}
+
+
+	@Bean
+	public DataSourceTransactionManager transactionManager(@Qualifier("dynamicDataSource") DataSource dynamicDataSource) {
+		return new DataSourceTransactionManager(dynamicDataSource);
+	}
+}
